32fd017c4c66c8bfd1c4e1ebac8f99559b711071
md4648 <mulunehdebebe66@gmail.com>
2025-02-25 23:25:29 +0300

    mysql database connection

--- Inventory_Management/settings.py
+++ Inventory_Management/settings.py
@@ -76,10 +76,19 @@ WSGI_APPLICATION = 'Inventory_Management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'stockmgnt',
+        'USER':'md',
+        'PASSWORD':'admin123'
     }
 }
 
